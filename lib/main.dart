30fad3064a7bbdab51f50be4da675acef395bd71
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-03-02 15:54:57 +0900

    add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:share_recipe/providers/auth_provider.dart';
+import 'package:share_recipe/screen/auth/page.dart';
 import 'package:share_recipe/screen/page.dart';
 import 'package:share_recipe/services/firebase_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FirebaseService.initialize();
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(authProvider);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
       ),
-      home: HomePage(),
+      home: user == null ? AuthPageMain() : RootPageMain(),
     );
   }
 }
