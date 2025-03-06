1c371716776e969cd0b2828e509cad0481a31c92
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-03-06 10:08:23 +0900

    add recipe model

---
+++ lib/models/recipe.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'recipe.freezed.dart';
+part 'recipe.g.dart';
+
+@freezed
+@JsonSerializable()
+class Recipe with _$Recipe {
+  const factory Recipe({
+    String? id,
+    required String title,
+    required List<String> ingredients,
+    required String description,
+    required String imageUrl,
+  }) = _Recipe;
+
+  factory Recipe.fromJson(Map<String, dynamic> json) => _$RecipeFromJson(json);
+}
---
+++ lib/models/recipe.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipe.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Recipe _$RecipeFromJson(Map<String, dynamic> json) => Recipe(
+  id: json['id'] as String?,
+  title: json['title'] as String,
+  ingredients:
+      (json['ingredients'] as List<dynamic>).map((e) => e as String).toList(),
+  description: json['description'] as String,
+  imageUrl: json['imageUrl'] as String,
+);
+
+Map<String, dynamic> _$RecipeToJson(Recipe instance) => <String, dynamic>{
+  'id': instance.id,
+  'title': instance.title,
+  'ingredients': instance.ingredients,
+  'description': instance.description,
+  'imageUrl': instance.imageUrl,
+};
+
+_$RecipeImpl _$$RecipeImplFromJson(Map<String, dynamic> json) => _$RecipeImpl(
+  id: json['id'] as String?,
+  title: json['title'] as String,
+  ingredients:
+      (json['ingredients'] as List<dynamic>).map((e) => e as String).toList(),
+  description: json['description'] as String,
+  imageUrl: json['imageUrl'] as String,
+);
+
+Map<String, dynamic> _$$RecipeImplToJson(_$RecipeImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'ingredients': instance.ingredients,
+      'description': instance.description,
+      'imageUrl': instance.imageUrl,
+    };
